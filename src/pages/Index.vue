59584f7858c42baf51b0f9c7821eca871a97d0ee
Mubarak SULAYMAN <mubaracksulaymann@gmail.com>
2023-02-18 19:05:14 +0000

    FIX: Minor clean up

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <p>Redirecting...</p>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+
+  created() {
+    this.$router.replace("/app/invoices");
+  },
+};
 </script>
